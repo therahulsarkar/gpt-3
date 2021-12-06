a16f2a5a1886e1da539d6fe053b40ba0a3adcab0
Rahul <thecoderrahul@gmail.com>
2021-12-07 04:26:16 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Article } from './article/article'
+export { default as Brand } from './brand/brand'
+export { default as CTA } from './callToAction/cta'
+export { default as Feature } from './feature/feature'
+export { default as Navbar } from './Navbar/Navbar'
+
